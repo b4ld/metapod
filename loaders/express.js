188295e92678dcb04bf0ff311c20e8c5f771b900
pedro_carmezim <pedro.carmezim@novabase.pt>
2020-04-18 21:23:24 +0100

    add new instructions page to /home

--- loaders/express.js
+++ loaders/express.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const containerRouter = require('./../api/routes/Container.route');
 const metadataRouter = require('./../api/routes/Metadata.route');
+const homeRouter = require('./../api/routes/Home.route');
 
 // const entryPage = require('./../entryPage.html');
 
@@ -11,13 +12,14 @@ const metadataRouter = require('./../api/routes/Metadata.route');
  * example: routes, cors, modules, express settings
  */
 const expressLoader = async ({ app }) => {
-  app.get('/', (req, res) => res.send('Hello - METAPOD IS WORKING'));
+  app.get('/', (req, res) => res.redirect('/home'));
   app.use(cors());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   
   app.use(containerRouter);
   app.use(metadataRouter);
+  app.use(homeRouter);
 
 
 
